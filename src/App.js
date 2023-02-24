fcaa455687e58e212a3bfc5bd56d4e802fd35498
m-shao <s201083816@ddsbstudent.ca>
2023-02-24 12:41:06 -0500

    navbar created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      PoogaType
+      <div className='h-screen w-screen bg-neutral-800 text-white text-5xl p-'>
+        <div className='flex w-full'>
+            <h1 className='font-black'>
+              Pooga<span className='text-indigo-400'>Type</span>
+            </h1>
+            <img src="./images/settings.svg" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
