baad0c4ac7910433755ae2f39f07a2959cd180a8
m-shao <s201083816@ddsbstudent.ca>
2023-02-25 00:45:53 -0500

    added word advancement, letter advancement wip

--- src/components/TypingField.jsx
+++ src/components/TypingField.jsx
@@ -1,18 +1,24 @@
 import React, { useState, useRef, useEffect } from 'react'
 
-function Letter({keyLetter}) {
+function Letter({keyLetter, correct}) {
     return(
         
-        <span>{keyLetter}</span>
+        <span className={
+            correct === 0 ? "text-red-400": (correct === 1? "text-white": "text-neutral-500")
+        }>{keyLetter}</span>
     )
 }
 
-function Word({keyWord}) {
+function Word({keyWord, active, curLetter, yesLetter}) {
     const wordList = keyWord.split("")
+    const [activeLetter, setActiveLetter] = useState(0)
+    
     return(
         <span>{wordList.map((letter, index) => {
             return(
-                <Letter keyLetter={letter} key={index} />
+                active? 
+                <Letter keyLetter={letter} key={index} correct={1}/> : 
+                <Letter keyLetter={letter} key={index} correct={0}/>
             )
         })} </span>
     )
@@ -22,6 +28,9 @@ function TypingField() {
     
     const paragraph = "and we need you for a little late night dinner and then i have a few days off work tomorrow night so we will have a plan on tuesday for you and i can just drop your off and bring it home to the house if i can come over to your mom house and i get some water for me and then i’ll take you out for me and then i’ll take"
     const paraList = paragraph.split(" ")
+    const [activeWord, setActiveWord] = useState(0)
+    const [curLetter, setCurLetter]= useState(-1)
+    const [yesLetter, setYesLetter]= useState('')
 
     const [text, setText] = useState('')
 
@@ -36,16 +45,25 @@ function TypingField() {
     }
 
     useEffect(() => {
-        console.log(text)
+        if (text[text.length - 1] === " "){    
+            setActiveWord(activeWord + 1)
+            setCurLetter(0)
+        } else{
+            console.log(curLetter)
+            setCurLetter(curLetter + 1)
+            setYesLetter(text[text.length - 1])
+        }
     }, [text])
     
+    let yes = null
     return (
         <>
             <div className="flex justify-center items-center w-full h-full">
                 <div className='max-w-3xl text-left text-2xl tracking-wide font-normal text-neutral-500 [word-spacing:5px]'>
                     {paraList.map((word, index) => {
+                        index === activeWord ? yes = true : yes = false
                         return(
-                            <Word keyWord={word} key={index} />        
+                            <Word keyWord={word} key={index} active={yes} curLetter={curLetter} yesLetter={yesLetter}/>
                         )
                     })}
                 </div>
