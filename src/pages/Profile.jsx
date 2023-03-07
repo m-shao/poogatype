5990c0c244ebd5f4112f9502bcbdccb235540539
m-shao <s201083816@ddsbstudent.ca>
2023-03-07 01:53:51 -0500

    small colour bug fix

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -66,15 +66,15 @@ function Profile() {
             <ul className='text-3xl flex flex-row gap-6 justify-between w-full'>
                 <li className='text-center'>
                     <h2>Average WPM:</h2>
-                    <h3 className='text-indigo-400 font-black text-5xl'>{parseInt(wpm)}</h3>
+                    <h3 className='text-[color:var(--highlight-primary)] font-black text-5xl'>{parseInt(wpm)}</h3>
                 </li>
                 <li className='text-center'>
                     <h2>Average Accuracy:</h2>
-                    <h3 className='text-indigo-400 font-black text-5xl'>{parseInt(acc)}%</h3>
+                    <h3 className='text-[color:var(--highlight-primary)] font-black text-5xl'>{parseInt(acc)}%</h3>
                 </li>
                 <li className='text-center'>
                     <h2>Games Played:</h2>
-                    <h3 className='text-indigo-400 font-black text-5xl'>{parseInt(games)}</h3>
+                    <h3 className='text-[color:var(--highlight-primary)] font-black text-5xl'>{parseInt(games)}</h3>
                 </li>
             </ul>
       </div>
