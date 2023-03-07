4fe72c3838c49182419b3fc5bd0fec13c03659cc
m-shao <s201083816@ddsbstudent.ca>
2023-03-07 07:20:27 -0500

    updated colour bugs

--- src/components/GreyedCard.jsx
+++ src/components/GreyedCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function GreyedCard({children, flexDir, gap, justify, align}) {
   return (
     <div className='w-screen h-screen flex items-center justify-center fixed left-0 top-0 bg-black bg-opacity-60 p-4'>
-        <div className={'bg-[color:var(--base-primary)] text-white max-h-128 h-full max-w-4xl w-full p-12 rounded-xl relative flex flex-'
+        <div className={'bg-[color:var(--base-primary)] text-[color:var(--text-primary)] max-h-128 h-full max-w-4xl w-full p-12 rounded-xl relative flex flex-'
         + flexDir + ' gap-' + gap + ' justify-' + justify + ' items-' + align}>
             {children}
         </div>
